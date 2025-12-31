abf2967118bfeb0cab9d55a80c26644cdad627dc
Royain Harriott <youngtallent1988@gmail.com>
2025-12-31 04:01:33 -0500

    Prepare app for Railway deployment

--- app.py
+++ app.py
@@ -129,5 +129,6 @@ def convert():
 )
 
 if __name__ == "__main__":
-   app.run(port=5001, debug=True)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port, debug=False)
 
